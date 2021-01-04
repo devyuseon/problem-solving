5d8923a0ba66124c7d08e8524aea8e782818aee1
yuseon-Lim <lys4486@gmail.com>
2021-01-04 18:22:36 +0900

    2020.01.04

---
+++ leetcode/344.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+
+        i = len(s) // 2
+
+        while i > 0:
+            s[i-1], s[len(s)-i] = s[len(s)-i], s[i-1]
+            i -= 1
