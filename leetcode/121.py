39975666bc0824e80990f10436ebe6ade272853c
yuseon-Lim <lys4486@gmail.com>
2021-01-11 11:57:35 +0900

    2021.01.11

--- leetcode/121.py
+++ leetcode/121.py
@@ -1,6 +1,33 @@
-import collections
-from typing import OrderedDict
 from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) > 1:
+            # 계속 상승하는 그래프일 경우를 위해 0 넣고 시작
+            low_point_index = [0]
+            profits = []
+
+            # 저점 인덱스 저장
+            for i in range(1, len(prices) - 1):
+                if (prices[i-1] > prices[i]):
+                    low_point_index.append(i)
+
+            # profit 구하기
+            for n in low_point_index:
+                tmp = max(prices[n+1:]) - prices[n]
+                if tmp > 0:
+                    profits.append(tmp)
+
+            if len(profits) > 0:
+                return max(profits)
+            else:
+                return 0
+
+        # len(prices)가 0,1인 경우 profit 0
+        else:
+            return 0
+
 
 """
 <브루트 포스 - 타임아웃>
@@ -16,15 +43,3 @@ class Solution:
         else:
             return max(profits)
 """
-
-
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        stock = {}
-        for i, n in enumerate(prices):
-            stock[i] = n
-
-        # 오름차순으로 정렬 , key기준
-        OrderedDict(stock.sort(stock.items(), lambda x: x[0]))
-
-        for i in range(len(prices), 0, -1):
