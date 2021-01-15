42a32dd1c76dc794ef74a3bcc5b96ca1ea1e69ac
yuseon-Lim <lys4486@gmail.com>
2021-01-15 13:04:12 +0900

    2021.01.15

---
+++ leetcode/92.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
+        # 예외 처리
+        if not head or m == n:
+            return head
+
+        root = start = ListNode(None)
+        root.next = head
+
+        for _ in range(m - 1):
+            start = start.next
+        end = start.next
+
+        for _ in range(n - m):
+            tmp = start.next
+            start.next = end.next
+            end.next = end.next.next
+            start.next.next = tmp
+
+        return root.next
