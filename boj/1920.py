4716761d12a5d47325f2dde7a05d77ea01499b4c
yuseon-Lim <lys4486@gmail.com>
2021-02-14 23:55:02 +0900

    2021.02.14

---
+++ boj/1920.py
@@ -0,0 +1,6 @@
+import sys
+
+N = int(input())
+list1 = [map(int,sys.stdin.readline().rstrip().split()) for _ in range(N)]
+M = int(input())
+list2 = [map(int,sys.stdin.readline().rstrip().split()) for _ in range(M)]
