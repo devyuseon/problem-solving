b162fdfab23e3ce23b2f825ff288eff66f4dc01e
yuseon-Lim <lys4486@gmail.com>
2021-02-19 00:57:47 +0900

    2021.02.18

--- boj/11723.py
+++ boj/11723.py
@@ -1,26 +1,31 @@
 import sys
 
 S = set()
+nums = [i for i in range(1,21)]
 
 N = int(input())
 for _ in range(N):
     command = sys.stdin.readline().split()
+
+    if len(command) == 1:
+        if command[0] == "all":
+            S.update(nums)
+        if command[0] == "empty":
+            S = set()
+        continue
+    
+    n = int(command[1])
     if command[0] == "add":
-        print(S.add(command[1])
-    elif command[0] == "remove":
-        print(S.remove(command[1])
-    elif command[0] == "check":
-        if command[1] in S:
+        S.add(n)
+    if command[0] == "remove":
+        S.discard(n)
+    if command[0] == "check":
+        if n in S:
             print(1)
         else:
             print(0)
-    elif command[0] == "toggle":
-        if command[1] in S:
-            S.remove(command[1])
-            S.add(command[1])
+    if command[0] == "toggle":
+        if n in S:
+            S.remove(n)
         else:
-            S.add(command[1])
-    elif command[0] == "all":
-        print(S.back())
-    elif command[0] == "empty":
-        print(S.back()) 
+            S.add(n)
