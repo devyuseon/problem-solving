202e49346e3aa7d2c832c4596cb56a7fb219bc13
임유선 <1991167@hansung.ac.kr>
2021-07-01 19:07:18 +0900

    속도 개선

--- boj/10818.py
+++ boj/10818.py
@@ -1,4 +1,23 @@
+"""
 N = int(input())
 nums = list(map(int,input().split()))
 print(min(nums), end=" ")
 print(max(nums))
+"""
+
+import sys
+
+N = int(input())
+
+nums = map(int,sys.stdin.readline().split())
+
+max = -sys.maxsize
+min = sys.maxsize
+
+for n in nums:
+    if n > max:
+        max = n
+    if n < min:
+        min = n
+
+print(min, max)
