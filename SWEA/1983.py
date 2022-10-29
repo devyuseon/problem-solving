2052299f1fe10658c4cf1dc17315551c4a772def
임유선 <1991167@hansung.ac.kr>
2022-10-29 21:48:45 +0900

    :art: 코드 변경

--- SWEA/1983.py
+++ SWEA/1983.py
@@ -1,17 +1,13 @@
 T = int(input())
+grade = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
 
 for test_case in range(1, T + 1):
-    n, m = map(int, input().split())
-    board = [[0 for _ in range(n + 1)]]
+    n, k = map(int, input().split())
+    student = []
     for _ in range(n):
-        board.append([0] + list(map(int, input().split())))
-    res = 0
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            board[i][j] = board[i-1][j] + board[i][j]
-
-    for i in range(1, n - m + 2):
-        for j in range(1, n - m + 2):
-            res = max(res, sum(board[i + m - 1][j: j + m]) - sum(board[i - 1][j: j + m]))
-
-    print(f'#{test_case} {res}')
+        a, b, c = map(int, input().split())
+        student.append(a * 0.35 + b * 0.45 + c * 0.2)
+    student_grade = student[k - 1]
+    student.sort(reverse=True)
+    student_idx = student.index(student_grade) // (n // 10)
+    print(f'#{test_case} {grade[student_idx]}')
