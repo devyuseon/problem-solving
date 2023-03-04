154cfe99609ba96181cd45ccf3a2af114a1b261e
yuseonLim <67352902+devyuseon@users.noreply.github.com>
2023-03-05 00:57:22 +0900

    [Gold II] Title: 공항, Time: 256 ms, Memory: 22748 KB -BaekjoonHub

---
+++ 백준/Gold/10775. 공항/공항.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+    public static int[] gates;
+
+    public static int find(int x) {
+        if (gates[x] != x) {
+            gates[x] = find(gates[x]);
+        }
+        return gates[x];
+    }
+
+    public static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x < y) gates[y] = x;
+        else gates[x] = y;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int g = Integer.parseInt(br.readLine());
+        int p = Integer.parseInt(br.readLine());
+        int res = 0;
+
+        gates = new int[g + 1];
+        for (int i = 0; i <= g; i++) {
+            gates[i] = i;
+        }
+
+        for (int i = 0; i < p; i++) {
+            int n = find(Integer.parseInt(br.readLine()));
+            if (n == 0) break;
+            union(n, n - 1);
+            res++;
+        }
+        System.out.println(res);
+    }
+}
