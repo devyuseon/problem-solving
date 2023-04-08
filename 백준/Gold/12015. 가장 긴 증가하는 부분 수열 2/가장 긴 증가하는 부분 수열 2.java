c00ef3fd4f703660a0bb9e4ce7392ee57ae2b64b
yuseonLim <67352902+devyuseon@users.noreply.github.com>
2023-04-08 23:52:53 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 540 ms, Memory: 95980 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int[] nums;
+    public static int[] lis;
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+
+    public static int binarySearch(int left, int right, int target) {
+        int mid;
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (lis[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return right;
+    }
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        nums = new int[n];
+        lis = new int [n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        lis[0] = nums[0];
+        int last = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (lis[last] < nums[i]) {
+                lis[last + 1] = nums[i];
+                last++;
+            } else {
+                int idx = binarySearch(0, last, nums[i]);
+                lis[idx] = nums[i];
+            }
+        }
+
+        System.out.println(last + 1);
+    }
+}
